af730832eb788cb38daaed176c046eb2ec3e5251
Jonah George <jonah.george@me.com>
2018-05-17 08:40:14 -0400

    [Fixes #22] Span tags overwrite for the same key

---
+++ tests/SpanTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Jaeger;
+
+use Jaeger\Reporter\NullReporter;
+use Jaeger\Sampler\ConstSampler;
+use PHPUnit\Framework\TestCase;
+
+class SpanTest extends TestCase
+{
+    private $tracer;
+    private $context;
+
+    function setUp()
+    {
+        $this->tracer = new Tracer('test-service', new NullReporter, new ConstSampler);
+        $this->context = new SpanContext(0, 0,0, SAMPLED_FLAG);
+    }
+
+    function testSetTag_TagsKeysAreUnique()
+    {
+        // Given
+        $span = new Span($this->context, $this->tracer, 'test-operation');
+
+        // When
+        $span->setTag('foo', 'test-component-1');
+        $span->setTag('foo', 'test-component-2');
+
+        // Then
+        $this->assertEquals( 1, count($span->getTags()));
+        $this->assertEquals( 'test-component-2', $span->getTags()['foo']->value);
+    }
+}
