74981cf069a2db81b38f1dd6786305ab148de3aa
Ben Long <14Benj@gmail.com>
2018-06-03 17:12:58 -0400

    Removing test risks by adding assertions/expectations

--- tests/Jaeger/Reporter/LoggingReporterTest.php
+++ tests/Jaeger/Reporter/LoggingReporterTest.php
@@ -8,13 +8,15 @@ use Psr\Log\NullLogger;
 
 class LoggingReporterTest extends TestCase
 {
-    public function testLoggingReporter()
+    public function testReportSpan()
     {
         $logger = $this->createMock(NullLogger::class);
         $span = $this->createMock(Span::class);
+
         $reporter = new LoggingReporter($logger);
 
-        $logger->method('info')
+        $logger->expects($this->once())
+            ->method('info')
             ->with($this->stringStartsWith('Reporting span'));
 
         $reporter->reportSpan($span);
