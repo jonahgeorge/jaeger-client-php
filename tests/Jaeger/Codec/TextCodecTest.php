a6c943381980f4555476137d46ed7aaa9861c726
Jonah George <jonah.george@me.com>
2018-06-06 09:14:36 -0400

    Move TextCodectest to correct location

--- tests/Jaeger/Codec/TextCodecTest.php
+++ tests/Jaeger/Codec/TextCodecTest.php
@@ -27,4 +27,27 @@ class TextCodecTest extends TestCase
 
         $this->assertFalse(empty($carrier));
     }
+
+    public function testSpanContextParsingFromHeader()
+    {
+        $carrier = ['uber-trace-id' => '32834e4115071776:f7802330248418d:32834e4115071776:1'];
+
+        $spanContext = $this->textCodec->extract($carrier);
+
+        self::assertEquals("1717370599544170", $spanContext->getTraceId());
+        self::assertEquals("641546560935337", $spanContext->getSpanId());
+        self::assertEquals("1717370599544170", $spanContext->getParentId());
+        self::assertEquals(1, $spanContext->getFlags());
+    }
+
+    /**
+     * @expectedException Exception
+     * @expectedExceptionMessage Malformed tracer state string
+     */
+    public function testInvalidSpanContextParsingFromHeader()
+    {
+        $carrier = ['uber-trace-id' => 'invalid_data'];
+
+        $this->textCodec->extract($carrier);
+    }
 }
