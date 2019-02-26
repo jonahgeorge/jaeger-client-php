13b5f5744ddc63ca02643b6fc5ffea1348586952
sfunk <sfunkhouser@appnexus.com>
2019-02-26 11:06:09 -0500

    Fix issue with traces above PHP_INT_MAX being logged as 7fffffffffffffff

--- src/Jaeger/Codec/TextCodec.php
+++ src/Jaeger/Codec/TextCodec.php
@@ -165,9 +165,9 @@ class TextCodec implements CodecInterface
         }
 
         return [
-            $this->hexToInt64($parts[0]),
-            $this->hexToInt64($parts[1]),
-            $this->hexToInt64($parts[2]),
+            CodecUtility::hexToInt64($parts[0]),
+            CodecUtility::hexToInt64($parts[1]),
+            CodecUtility::hexToInt64($parts[2]),
             $parts[3],
         ];
     }
@@ -182,39 +182,5 @@ class TextCodec implements CodecInterface
     private function startsWith(string $haystack, string $needle): bool
     {
         return substr($haystack, 0, strlen($needle)) == $needle;
-    }
-
-    /**
-     * Incoming trace/span IDs are hex representations of 64-bit values. PHP
-     * represents ints internally as signed 32- or 64-bit values, but base_convert
-     * converts to string representations of arbitrarily large positive numbers.
-     * This means at least half the incoming IDs will be larger than PHP_INT_MAX.
-     *
-     * Thrift, while building a binary representation of the IDs, performs bitwise
-     * operations on the string values, implicitly casting to int and capping them
-     * at PHP_INT_MAX. So, incoming IDs larger than PHP_INT_MAX will be serialized
-     * and sent to the agent as PHP_INT_MAX, breaking trace/span correlation.
-     *
-     * This method therefore, on 64-bit architectures, splits the hex string into
-     * high and low values, converts them separately to ints, and manually combines
-     * them into a proper signed int. This int is then handled properly by the
-     * Thrift package.
-     *
-     * On 32-bit architectures, it falls back to base_convert.
-     *
-     * @param string $hex
-     * @return string|int
-     */
-    private function hexToInt64($hex)
-    {
-        // If we're on a 32-bit architecture, fall back to base_convert.
-        if (PHP_INT_SIZE === 4) {
-            return base_convert($hex, 16, 10);
-        }
-
-        $hi = intval(substr($hex, -16, -8), 16);
-        $lo = intval(substr($hex, -8, 8), 16);
-
-        return $hi << 32 | $lo;
     }
 }
