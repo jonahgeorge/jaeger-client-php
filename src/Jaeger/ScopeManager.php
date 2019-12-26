e61a4c36b64fd9e0a4260dc81c44fd3b24fbc8a0
Alexey Shokov <alexey@shockov.com>
2019-12-26 12:02:43 +0400

    Support for further opentracing lib versions

--- src/Jaeger/ScopeManager.php
+++ src/Jaeger/ScopeManager.php
@@ -19,7 +19,7 @@ class ScopeManager implements OTScopeManager
     /**
      * {@inheritdoc}
      */
-    public function activate(OTSpan $span, $finishSpanOnClose)
+    public function activate(OTSpan $span, $finishSpanOnClose = true)
     {
         $this->active = new Scope($this, $span, $finishSpanOnClose);
 
