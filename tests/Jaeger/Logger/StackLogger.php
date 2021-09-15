1f1462a0ac40d474e248c70ab0d19bf676e0e63c
Alexander Pekhota <alex@pdffiller.com>
2021-09-15 11:35:52 +0300

    replaced thrown exceptions logic with logger->warning calls for ThriftUdpTransport

---
+++ tests/Jaeger/Logger/StackLogger.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Jaeger\Tests\Logger;
+
+use Psr\Log\LoggerTrait;
+
+class StackLogger implements \Psr\Log\LoggerInterface
+{
+    /** @var array */
+    protected $messagesStack = [];
+
+    use LoggerTrait;
+
+    public function log($level, $message, array $context = array())
+    {
+        $this->messagesStack[] = $message;
+    }
+
+    public function getLastMessage() {
+        return array_pop($this->messagesStack);
+    }
+
+    public function getMessagesCount() {
+        return count($this->messagesStack);
+    }
+
+    public function clear() {
+        $this->messagesStack = [];
+    }
+}
